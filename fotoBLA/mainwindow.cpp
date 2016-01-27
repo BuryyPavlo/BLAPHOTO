a4df946bfd882d88b52e0ff6bb5b0cf2f7ac9f1c
Buryi <pavlo.buryy@gmail.com>
2016-01-27 18:33:25 +0200

    add button openTXT

--- fotoBLA/mainwindow.cpp
+++ fotoBLA/mainwindow.cpp
@@ -1,5 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtGui>
+#include <QString>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QFile>
+#include <QTextEdit>
+#include  <cstring>
+#include <QChar>
+#include <QStringList>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +21,31 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_openTxt_clicked()
+{// вибір файлу через діалог
+    QString fileName = QFileDialog::getOpenFileName(this,QString::fromUtf8("Відкрити файл"),QDir::currentPath(),
+                                                    "*txt file (*.txt);;");
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly))
+        {
+           QMessageBox::critical(this,"Помилка ",QString::fromUtf8("Помилка читання файлу!"),
+                                 QMessageBox::Ok);
+            return;
+        }
+    QTextStream stream(&file);
+    QStringList str;
+    int numberRows=0;
+    while (!stream.atEnd())
+    {
+        str.append(stream.readLine());
+        numberRows++;
+    }
+    QMessageBox::critical(this,"file ", QString::number(numberRows),QMessageBox::Ok);
+    int i=0;
+    for (i=0; i<=numberRows; i++){
+    QMessageBox::information(this,"file ", str.at(i),QMessageBox::Ok);
+    }
+
 }
